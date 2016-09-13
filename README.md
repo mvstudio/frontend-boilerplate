e1eb7e6cbc469297e105f165c4b0a375f7e4d99c
Lou Verdun <verdun.lou@gmail.com>
2016-09-13 13:31:37 +0200

    Added Susy & Normalize.css / update gulp Js tasks

--- README.md
+++ README.md
@@ -3,16 +3,21 @@ Basic setup for project startup
 
 Works with npm and Gulp
 
+Includes Susy & Normalize.css
+
 To start the project, go to your project folder and run 'npm install' in your terminal
 
 To start watching files for changes, go to your project folder and run 'gulp' in your terminal
 
 # CSS
 * Compiles Sass and modules (libSass)
-* Adds vendor prefixes for browser support (postCss-autoprefixer)
-* Clean up and minify (postCss-clean)
+* Adds vendor prefixes for browser support (autoprefixer)
+* Clean up and minify files (postCss-clean)
+* Create sourcemaps (gulp-sourcemaps)
 
 # Javascript
 * Compiles ES6 for browser support with Babel (gulp-babel, babel-preset-es2015)
 * Concatenate files (gulp-concat)
+* Renames files to .min (gulp-rename)
+* Minify files (gulp-uglify)
 * Create sourcemaps (gulp-sourcemaps)
--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-// Configuration
+// CONFIGURATION
 var path = {
     css : "css/",
     sass : "css/",
@@ -7,40 +7,59 @@ var path = {
     maps: "maps/"
 };
 
-// Imports
+// IMPORTS
 var gulp = require("gulp");
+var concat = require("gulp-concat");
+var uglify = require("gulp-uglify");
+var rename = require("gulp-rename");
 var sourcemaps = require('gulp-sourcemaps');
 
+// Sass
 var sass = require("gulp-sass");
 var postCSS = require('gulp-postcss');
 var cleanCSS = require('postcss-clean');
 var autoprefixer = require('autoprefixer');
+var normalize = require('node-normalize-scss').includePaths;
 
+// Grids
+var susy = 'node_modules/susy/sass';
+
+// Javascript
 var babel = require("gulp-babel");
-var concat = require("gulp-concat");
 
-// Scripts
+// TASKS
+// Sass / css
 gulp.task('sass', function() {
     gulp.src(path.sass+"*.scss")
-        .pipe(sass({outputStyle: 'nested'}).on("error", sass.logError))
-        .pipe(postCSS([
-            autoprefixer({ browsers: ['>1%', 'last 10 versions', 'IE 9'] }),
-            cleanCSS({compatibility: 'ie9'})
-        ]))
+        .pipe(sourcemaps.init())
+            .pipe(sass({
+                includePaths: [].concat(normalize, susy),
+                outputStyle: 'nested'
+            }).on("error", sass.logError))
+            .pipe(postCSS([
+                autoprefixer({ browsers: ['>1%', 'last 10 versions', 'IE 9'] }),
+                cleanCSS({compatibility: 'ie9'})
+            ]))
+        .pipe(sourcemaps.write(path.maps))
         .pipe(gulp.dest(path.css));
 });
 
+// Javascript
 gulp.task('js', function() {
     gulp.src(path.js+"*.js")
-        .pipe(sourcemaps.init({loadMaps: true}))
+        .pipe(sourcemaps.init())
             .pipe(babel({
                 presets: ["ES2015"]
             }))
-            .pipe(concat('app-dist.js'))
+            .pipe(concat('app.js'))
+            .pipe(gulp.dest(path.jsDist))
+            .pipe(rename('app.min.js'))
+            .pipe(uglify())
             .pipe(sourcemaps.write(path.maps))
         .pipe(gulp.dest(path.jsDist));
 });
 
+// WATCHES
 gulp.task('default', function() {
     gulp.watch(path.sass + "*.scss", ["sass"]);
     gulp.watch(path.js + "*.js", ["js"]);
